d7b861a5a74d3f0bacc991b60073d4d784b89ee4
philippe.d <philippe.dan@sortlist.com>
2023-02-20 14:06:09 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ChartComponent } from "./components/ChartComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 w-screen h-screen flex flex-row">
+      <div className="bg-white w-[300px] p-6 space-y-6">
+        {/* sidebar */}
+        <div className="font-bold text-xl">Network Performance Framework</div>
+        <ul className="space-y-2">
+          <li className="font-bold">Home</li>
+          <li>Device 1</li>
+          <li>Device 2</li>
+          <li>Device 3</li>
+          <li>Device 4</li>
+        </ul>
+      </div>
+      <div className="p-6">
+        <div className="flex flex-row gap-6">
+          {[1, 2, 3, 4].map((index) => (
+            <div className="bg-white rounded-xl p-6 space-y-2" key={index}>
+              <div>Experiments count</div>
+              <div className="mx-auto w-max text-blue-500">17</div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <ChartComponent></ChartComponent>
+        </div>
+      </div>
+      {/* Main page */}
     </div>
   );
 }
---
+++ src/components/ChartComponent.tsx
@@ -0,0 +1,25 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ["Red", "Blue", "Yellow"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [300, 50, 100],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+export const ChartComponent = () => {
+  return <Doughnut data={data} />;
+};
