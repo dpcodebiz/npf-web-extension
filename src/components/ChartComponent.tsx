bba29f59301961f070af3d3ecd20a78ccb697293
dpcodebiz <43727605+dpcodebiz@users.noreply.github.com>
2023-02-26 19:54:09 +0100

    feat: wip parsing for poc

--- src/components/ChartComponent.tsx
+++ src/components/ChartComponent.tsx
@@ -1,25 +1,81 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  ChartData,
+} from "chart.js";
 import React from "react";
-import { Doughnut } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
-const data = {
-  labels: ["Red", "Blue", "Yellow"],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [300, 50, 100],
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)",
-      ],
-      hoverOffset: 4,
+const raw_data = {
+  "01-iperf.npf": {
+    "('', 'without')": {
+      "1": 60501974220.8,
+      "2": 51092413440.0,
+      "3": 76334086963.2,
+      "4": 64968509849.6,
+      "5": 57515010252.8,
+      "6": 58613023744.0,
+      "7": 67958106112.0,
+      "8": 50650913177.6,
     },
-  ],
+    "('-Z', 'with')": {
+      "1": 92964336640.0,
+      "2": 93229826867.2,
+      "3": 100535490355.2,
+      "4": 98984001331.2,
+      "5": 98882097561.6,
+      "6": 98175769190.4,
+      "7": 99427311411.2,
+      "8": 94954852966.4,
+    },
+  },
+};
+
+const getLabel = () => {
+  return Object.entries(raw_data["01-iperf.npf"]).map(([k, pairs]) => Object.keys(pairs));
+};
+
+const getData = () => {
+  return Object.entries(raw_data["01-iperf.npf"]).map(([k, v]) => ({
+    label: k,
+    data: Object.values(v),
+    backgroundColor: "blue",
+    borderColor: "blue",
+  }));
+};
+
+const data: ChartData<"line", number[], string> = {
+  labels: getLabel()[0],
+  datasets: getData(),
 };
 
 export const ChartComponent = () => {
-  return <Doughnut data={data} />;
+  console.log(getLabel(), getData());
+
+  return (
+    <Line
+      data={data}
+      options={{
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top" as const,
+          },
+          title: {
+            display: true,
+            text: Object.keys(raw_data)[0],
+          },
+        },
+      }}
+    />
+  );
 };
