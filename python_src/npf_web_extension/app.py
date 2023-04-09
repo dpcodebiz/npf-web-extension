c2cdd9ca7ca22e680773beeda869a571642a512c
dpcodebiz <43727605+dpcodebiz@users.noreply.github.com>
2023-04-10 01:13:43 +0200

    feat: cleaned up and added proper interface

--- python_src/npf_web_extension/app.py
+++ python_src/npf_web_extension/app.py
@@ -1,3 +1,5 @@
+import json
+import shutil
 import sys
 from argparse import ArgumentParser
 from importlib.metadata import version
@@ -14,5 +16,36 @@ def main():
         sys.exit()
 
 
-def hydrate_app():
-    print("Hydrated")
+def _prepare(outdir):
+
+    # Configuration
+    file_path = "index.html"
+
+    # Copying app to outdir
+    shutil.copy(file_path, f"{outdir}/{file_path}")
+
+    
+def _hydrate(configuration, outdir):
+
+    # Configuration
+    file_path = f"{outdir}/index.html"
+    insertion_point = "<!-- NPF_CONFIG_INSERTION -->"
+    js_snippet = f'\t<script type="text/javascript">setTimeout(() => window.updateConfiguration({json.dumps(configuration)}), 2500)</script>'
+
+    # Replacing the insertion point with the js_snippet
+    with open(file_path, 'r') as file:
+        file_contents = file.read()
+
+    new_contents = file_contents.replace(insertion_point, js_snippet)
+
+    with open(file_path, 'w') as file:
+        # Write the modified contents to the file
+        file.write(new_contents)
+
+
+def export(configuration, outdir):
+    
+    _prepare(outdir)
+    _hydrate(configuration, outdir)
+
+    print("Web app has been exported properly")
