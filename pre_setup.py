1692c6988924e5f4fd3586e58fb797a21d39555d
dpcodebiz <43727605+dpcodebiz@users.noreply.github.com>
2023-04-10 19:26:30 +0200

    fix: re-added copyfiles package instead of prebuild script

--- pre_setup.py
+++
@@ -1,8 +0,0 @@
-import shutil
-
-# Configuration
-file_path = "build/index.html"
-python_file_path = "python_src/npf_web_extension/template.py"
-
-# Copy template file
-shutil.copyfile(file_path, python_file_path)
