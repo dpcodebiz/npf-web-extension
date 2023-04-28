3f76136f49279511675e580191243f97e8ccd586
dpcodebiz <43727605+dpcodebiz@users.noreply.github.com>
2023-04-28 21:37:49 +0200

    refactor: extract graph splitting methods

--- src/utils/configuration/parser.ts
+++ src/utils/configuration/parser.ts
@@ -1,9 +1,8 @@
 import { ParseResult } from "papaparse";
-import { Configuration, ConfigurationData, GRAPH_TYPES, Settings, SplitParameter, SplitParametersData } from "./types";
+import { Configuration, ConfigurationData, GRAPH_TYPES, Settings, SplitParametersData } from "./types";
 import { getRecommendedGraphType } from "./data_analyzer";
-import { getLineChartConfiguration, groupDataByParameters } from "./parser/line";
-import { splitParams } from "./utils";
-import { mapValues, shake, unique } from "radash";
+import { getLineChartConfiguration } from "./parser/line";
+import { getConfigurationDataByParameters } from "./utils";
 
 export type ParsedConfigurationData = { [index: string]: string };
 export type ExperimentData = { [index: string]: number };
@@ -13,100 +12,15 @@ export function resultsToConfiguration(
   results: ParseResult<ParsedConfigurationData>,
   settings: Settings
 ): Configuration {
-  // Checking how many parameters vary
-  const getParametersValues = (dataGroupedByParameters: any) => {
-    const keysParametersJoined = Object.keys(dataGroupedByParameters);
-    const arrayOfParametersSplit = keysParametersJoined.map((joinedParams) => splitParams(joinedParams));
-    const parameters = Object.keys(arrayOfParametersSplit[0]);
-
-    // Getting values for each parameter
-    const valuesForEachParameter = Object.assign(
-      {},
-      ...parameters.map((parameter) => ({
-        [parameter]: arrayOfParametersSplit.reduce<any>((acc, val) => acc.concat([val[parameter]]), []),
-      }))
-    );
-    const uniqueValuesForEachParameter = mapValues(valuesForEachParameter, (values: any) => unique(values));
-
-    return uniqueValuesForEachParameter;
-  };
-
-  const getChangingParameters = (unique = true) => {
-    const dataGrouped = groupDataByParameters(configurationData.parameters, results.data);
-    const valuesForEachParameter = getParametersValues(dataGrouped);
-    const changingParameters = shake(valuesForEachParameter, (values) => values.length < 2);
-
-    console.log(getParametersValues(dataGrouped), valuesForEachParameter, changingParameters);
-
-    return unique ? changingParameters : valuesForEachParameter;
-  };
-
-  const changingParameters = getChangingParameters(false);
-  const nChangingParameters = Object.keys(changingParameters).length;
-
-  const splitParsedConfigurationData: {
-    split_parameters?: SplitParametersData;
-    data: ParsedConfigurationData[];
-  }[] = [];
-
-  // Split along X axis
-  if (nChangingParameters > 2) {
-    const [changingParameterX, valuesX] = Object.entries(changingParameters)[2];
-
-    // Split along Y axis
-    if (nChangingParameters > 3) {
-      const [changingParameterY, valuesY] = Object.entries(changingParameters)[3];
-
-      for (const valueX of valuesX) {
-        for (const valueY of valuesY) {
-          const resultsByChangingParameter = results.data.filter(
-            (data) => data[changingParameterX] == valueX && data[changingParameterY] == valueY
-          );
-
-          splitParsedConfigurationData.push({
-            split_parameters: {
-              x: {
-                name: changingParameterX,
-                values: valuesX as any,
-              },
-              y: {
-                name: changingParameterY,
-                values: valuesY as any,
-              },
-            },
-            data: resultsByChangingParameter,
-          });
-        }
-      }
-    } else {
-      for (const valueX of valuesX) {
-        const resultsByChangingParameter = results.data.filter((data) => data[changingParameterX] == valueX);
-
-        splitParsedConfigurationData.push({
-          split_parameters: {
-            x: {
-              name: changingParameterX,
-              values: valuesX as any,
-            },
-          },
-          data: resultsByChangingParameter,
-        });
-      }
-    }
-
-    console.log("multivariate!", splitParsedConfigurationData);
-  } else {
-    splitParsedConfigurationData.push({
-      data: results.data,
-    });
-  }
+  // TODO remove main param?
+  const configurationDataByParameters = getConfigurationDataByParameters(configurationData.parameters, results.data);
 
   // Preparing configuration
   const configuration: Configuration = {
     experiments: [],
   };
 
-  splitParsedConfigurationData.forEach((data) => {
+  configurationDataByParameters.forEach((data) => {
     const { split_parameters, data: resultsByChangingParameter } = data;
 
     // Analyzing data and getting recommended graph type for this data
--- src/utils/configuration/utils.ts
+++ src/utils/configuration/utils.ts
@@ -1,9 +1,25 @@
-import { Experiment } from "./types";
+import { isArray, mapValues, shake, unique } from "radash";
+import { ParsedConfigurationData } from "./parser";
+import { groupDataByParameters } from "./parser/line";
+import { Experiment, SplitParametersData } from "./types";
 
-export const joinParams = (params_to_join: string[], params: { [index: string]: string }) => {
+/**
+ * Joins all parameters
+ * @param params_to_join List of parameters to join together
+ * @param params
+ * @returns
+ */
+export const joinParams = (params_to_join: string[], params: ParsedConfigurationData) => {
   return params_to_join.reduce((acc, param) => `${acc}${param}=${params[param]},`, "");
 };
 
+/**
+ * Splits all parameters into a new Object.
+ *
+ * "var1=a,var2=b" => { "var1": "a", "var2": "b" }
+ * @param joined_params
+ * @returns
+ */
 export const splitParams = (joined_params: string) => {
   const separated: { [index: string]: string } = {};
   joined_params.split(",").map((paramWithValue) => {
@@ -27,3 +43,136 @@ export const getSplitParameters = (experiments: Experiment[]) => ({
     value,
   })),
 });
+
+/**
+ * Groups all data by parameters then iterates over all sets and returns all values found for each parameter
+ * @param dataGroupedByParameters
+ * @returns
+ */
+const getParametersValues = (dataGroupedByParameters: ReturnType<typeof groupDataByParameters>) => {
+  const keysParametersJoined = Object.keys(dataGroupedByParameters);
+  const arrayOfParametersSplit = keysParametersJoined.map((joinedParams) => splitParams(joinedParams));
+  const parameters = Object.keys(arrayOfParametersSplit[0]);
+
+  // Getting values for each parameter
+  const valuesForEachParameter = Object.assign(
+    {},
+    ...parameters.map((parameter) => ({
+      [parameter]: arrayOfParametersSplit.reduce<string[]>((acc, val) => acc.concat([val[parameter]]), []),
+    }))
+  );
+
+  // Remove duplicate values
+  const uniqueValuesForEachParameter = mapValues(valuesForEachParameter, (values: string[]) => unique(values));
+
+  return uniqueValuesForEachParameter;
+};
+
+/**
+ * Returns all parameters along with their possible values found inside the data
+ * @param parameters Acts as a filter for the parameters returned
+ * @param parsedConfigurationData
+ * @param changing If true, will return only parameters where the values change
+ * @returns
+ */
+export const getParametersWithValues = (
+  parameters: string[],
+  parsedConfigurationData: ParsedConfigurationData[],
+  changing = false
+) => {
+  const dataGrouped = groupDataByParameters(parameters, parsedConfigurationData);
+  const valuesForEachParameter = getParametersValues(dataGrouped);
+  const changingParameters = shake(valuesForEachParameter, (values) => values.length < 2);
+
+  return changing ? changingParameters : valuesForEachParameter;
+};
+
+/**
+ * Returns subsets of ParsedConfigurationData[] where each subset is has a specific combination of parameters values.
+ * @param parameters
+ * @param parsedConfigurationData
+ * @returns
+ */
+export const getConfigurationDataByParameters = (
+  parameters: string[],
+  parsedConfigurationData: ParsedConfigurationData[]
+) => {
+  // Getting all parameters with their values
+  const parametersWithValues = getParametersWithValues(parameters, parsedConfigurationData);
+  const nParameters = Object.keys(parametersWithValues).length;
+
+  const splitParsedConfigurationData: {
+    split_parameters?: SplitParametersData;
+    data: ParsedConfigurationData[];
+  }[] = [];
+
+  // Split along X axis
+  if (nParameters > 2) {
+    const [parameterX, valuesX] = Object.entries(parametersWithValues)[2];
+    const [parameterY, valuesY] = nParameters > 3 ? Object.entries(parametersWithValues)[3] : [];
+    const splitY = nParameters > 3 && parameterY && valuesY;
+
+    // Filtering data by parameters
+    const configurationDataByParameters = valuesX.map((valueX) =>
+      splitY
+        ? valuesY.map((valueY) =>
+            parsedConfigurationData.filter((data) => data[parameterX] == valueX && data[parameterY] == valueY)
+          )
+        : parsedConfigurationData.filter((data) => data[parameterX] == valueX)
+    );
+
+    // Iterating through all possible combinations
+    configurationDataByParameters.forEach((subResultsX) => {
+      if (isArray(subResultsX[0])) {
+        (subResultsX as ParsedConfigurationData[][]).forEach((subResultsXY) => {
+          splitParsedConfigurationData.push({
+            split_parameters: {
+              x: {
+                name: parameterX,
+                values: valuesX as string[],
+              },
+              ...(parameterY && valuesY
+                ? {
+                    y: {
+                      name: parameterY,
+                      values: valuesY as string[],
+                    },
+                  }
+                : {}),
+            },
+            data: subResultsXY,
+          });
+        });
+        return;
+      }
+
+      splitParsedConfigurationData.push({
+        split_parameters: {
+          x: {
+            name: parameterX,
+            values: valuesX as string[],
+          },
+          ...(parameterY && valuesY
+            ? {
+                y: {
+                  name: parameterY,
+                  values: valuesY as string[],
+                },
+              }
+            : {}),
+        },
+        data: subResultsX as ParsedConfigurationData[],
+      });
+    });
+
+    // Debug
+    console.log("multivariate!", splitParsedConfigurationData);
+  } else {
+    // No need to split the data so just pushing the data
+    splitParsedConfigurationData.push({
+      data: parsedConfigurationData,
+    });
+  }
+
+  return splitParsedConfigurationData;
+};
