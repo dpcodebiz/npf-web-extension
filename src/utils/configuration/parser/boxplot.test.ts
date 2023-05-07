8587425aee7e4796b186dcb4b624f444afee0adc
dpcodebiz <43727605+dpcodebiz@users.noreply.github.com>
2023-05-07 03:38:48 +0200

    feat boxplot tests

---
+++ src/utils/configuration/parser/boxplot.test.ts
@@ -0,0 +1,48 @@
+import { ConfigurationData } from "../types";
+import { getBoxPlotChartConfiguration } from "./boxplot";
+
+// Mock
+const configurationDataSimple: ConfigurationData = {
+  id: "02",
+  parameters: ["a"],
+  measurements: ["b"],
+  name: "",
+  data: ``,
+};
+const arrayOfParsedConfigurationData = [
+  {
+    a: "1",
+    b: "3",
+  },
+  {
+    a: "2",
+    b: "3",
+  },
+  {
+    a: "3",
+    b: "3",
+  },
+];
+
+test("getBoxPlotChartConfiguration", () => {
+  expect(getBoxPlotChartConfiguration({}, configurationDataSimple, arrayOfParsedConfigurationData)).toMatchObject({
+    main_parameter: "a",
+    metadata: {
+      recommended_type: 3,
+      type: 3,
+    },
+    name: "",
+    runs: [
+      {
+        parameters: {},
+        results: {
+          b: {
+            "1": [3],
+            "2": [3],
+            "3": [3],
+          },
+        },
+      },
+    ],
+  });
+});
